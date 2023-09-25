72474bdc4d66c975c2cadb28064045d544b6eb05
toan.pham2 <toan.pham2@isobar.com>
2023-09-25 17:18:27 +0700

    fixed metadata post page

--- src/app/blogs/[id]/page.tsx
+++ src/app/blogs/[id]/page.tsx
@@ -1,4 +1,5 @@
 import PostDetail from "@/modules/blogs/components/post-detail";
+import type { Metadata, ResolvingMetadata } from "next";
 
 // eslint-disable-next-line @next/next/no-async-client-component
 const BlogDetail = async () => {
@@ -9,3 +10,26 @@ const BlogDetail = async () => {
   );
 };
 export default BlogDetail;
+
+type Props = {
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const id = params.id;
+  // fetch data
+  const blogs = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}`
+  ).then((res) => {
+    return res.json();
+  });
+
+  return {
+    title: blogs.name,
+    description: blogs.body,
+  };
+}
